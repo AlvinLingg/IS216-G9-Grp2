117b016895ad101be00e4d04a6b8179c2f464ca2
Timothy Tay <timothy.tay.2021@scis.smu.edu.sg>
2022-10-11 22:47:41 +0800

    updated CuisineCard.vue

--- components/CuisineCard.vue
+++ components/CuisineCard.vue
@@ -1,12 +1,12 @@
 <template>
-    <div class="max-w-60 mx-auto">
-    <div class="rounded-3xl w-full">
+    <div class="w-60 mx-auto">
+      <div class="rounded-3xl w-full">
         <img
-            class="object-cover rounded-3xl"
-            src="../assets/test-img.jpg"
-            alt=""
+          class="object-cover rounded-3xl"
+          src="../assets/test-img.jpg"
+          alt=""
         />
-    </div>
+      </div>
     <h3 class="mt-4 font-semibold text-xl">Mediterranean</h3>
     </div>
 </template>
