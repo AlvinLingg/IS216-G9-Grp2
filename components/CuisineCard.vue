a67887cf5dc91e93d29ec2d4d26449b7181e7ec1
Timothy Tay <timothy.tay.2021@scis.smu.edu.sg>
2022-10-11 22:39:08 +0800

    add cuisine card and cuisine carousel components

---
+++ components/CuisineCard.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="max-w-60 mx-auto">
+    <div class="rounded-3xl w-full">
+        <img
+            class="object-cover rounded-3xl"
+            src="../assets/test-img.jpg"
+            alt=""
+        />
+    </div>
+    <h3 class="mt-4 font-semibold text-xl">Mediterranean</h3>
+    </div>
+</template>
+  
+<script>
+export default {};
+</script>
+  
+<style></style>
+  
---
+++ components/CuisineCarousel.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="w-4/5 mx-auto flex flex-col justify-center">
+  <h1 class="mt-4 font-semibold text-xl">Browse by Cuisine</h1>
+
+  <div class="carousel w-full mt-5">
+    <div id="slide1" class="carousel-item relative w-full">
+      <div class="w-full mx-auto px-20 flex flex-row">
+        <CuisineCard/>
+        <CuisineCard/>
+        <CuisineCard/>
+      </div>
+      <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+        <a href="#slide4" class="btn btn-circle">❮</a> 
+        <a href="#slide2" class="btn btn-circle">❯</a>
+      </div>
+    </div> 
+    <div id="slide2" class="carousel-item relative w-full">
+      <div class="w-full mx-auto px-20 flex flex-row">
+        <CuisineCard/>
+        <CuisineCard/>
+        <CuisineCard/>
+      </div>
+      <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+        <a href="#slide1" class="btn btn-circle">❮</a> 
+        <a href="#slide3" class="btn btn-circle">❯</a>
+      </div>
+    </div> 
+    <div id="slide3" class="carousel-item relative w-full">
+      <div class="w-full mx-auto px-20 flex flex-row">
+        <CuisineCard/>
+        <CuisineCard/>
+        <CuisineCard/>
+      </div>
+      <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+        <a href="#slide2" class="btn btn-circle">❮</a> 
+        <a href="#slide4" class="btn btn-circle">❯</a>
+      </div>
+    </div> 
+    <div id="slide4" class="carousel-item relative w-full">
+      <div class="w-full mx-auto px-20 flex flex-row">
+        <CuisineCard/>
+        <CuisineCard/>
+        <CuisineCard/>
+      </div>  
+      <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+        <a href="#slide3" class="btn btn-circle">❮</a> 
+        <a href="#slide1" class="btn btn-circle">❯</a>
+      </div>
+    </div>
+  </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
