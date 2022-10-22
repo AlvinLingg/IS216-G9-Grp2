768af3af876ed9789275b369d5fadad474648e7d
BY <9499796+boonyeow@users.noreply.github.com>
2022-10-23 04:30:44 +0800

    bugfix

--- pages/search.vue
+++ pages/search.vue
@@ -1,16 +1,21 @@
 <template>
   <div>
-    <Navbar />
     <div class="section p-5 max-w-6xl m-auto">
       <div class="flex flex-col gap-5">
-        <h1 class="text-5xl font-bold">Search Results</h1>
+        <div>
+          <h1 class="text-5xl font-bold">Search recipes</h1>
+          <p v-if="searchResults !== null" class="mt-2">{{searchResults.totalResults}} Results found</p>
+        </div>
         <div class="grid gap-6 mt-2 lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 xs:grid-cols-1">
 
 
           <template v-if="searchResults !== null">
-            <div v-for="recipe in Object.values(searchResults.results)">
-              <RecipeCard :recipe="recipe" @click="navigateTo(`/recipes/${recipe.id}`)" />
+            <div v-if="searchResults.totalResults !== 0">
+              <div v-for="recipe in Object.values(searchResults.results)">
+                <RecipeCard :recipe="recipe" @click="navigateTo(`/recipes/${recipe.id}`)" />
+              </div>
             </div>
+            <span v-else class="text-slate-600">No recipes found.</span>
           </template>
           <template v-else>
             <span class="text-slate-600">No recipes found.</span>
