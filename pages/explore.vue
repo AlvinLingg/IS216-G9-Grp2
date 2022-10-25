1e9c723cac002cf5b931e542176570d97be75a7f
BY <9499796+boonyeow@users.noreply.github.com>
2022-10-25 23:37:16 +0800

    fixed carousel, responsiveness + hide search bar on md

--- pages/explore.vue
+++ pages/explore.vue
@@ -3,7 +3,7 @@
     <div class="section p-10 max-w-6xl m-auto">
         <h1 class="text-5xl font-bold">Browse recipes</h1>
         <div class="flex mt-2 justify-between">
-            <div class="flex">
+            <div class="flex flex-row">
                 <p class="text-xl font-semibold text-slate-600  self-center mr-3">Filter by</p>
                 <div class="dropdown dropdown-end inline-block">
                     <label tabindex="0" class="btn m-1 grey-color rounded-3xl px-5" @click="toggleDropdown" @blur="">
@@ -12,7 +12,7 @@
                     <ul tabindex="0" class="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52">
                         <li v-for="intolerance in intolerances">
                             <label class="label cursor-pointer">
-                                <span class="label-text">{{intolerance}}</span>
+                                <span class="label-text">{{ intolerance }}</span>
                                 <input type="checkbox" class="checkbox" :value="intolerance"
                                     v-model="intoleranceFilter" />
                             </label>
@@ -25,13 +25,13 @@
                     </label>
                     <ul tabindex="0" class="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52">
                         <li>
-                            <label class="label cursor-pointer" @click="cuisineFilter=[]">
+                            <label class="label cursor-pointer" @click="cuisineFilter = []">
                                 Clear All
                             </label>
                         </li>
                         <li v-for="cuisine in cuisines">
                             <label class="label cursor-pointer">
-                                <span class="label-text">{{cuisine}}</span>
+                                <span class="label-text">{{ cuisine }}</span>
                                 <input type="checkbox" class="checkbox" :value="cuisine" v-model="cuisineFilter" />
                             </label>
                         </li>
@@ -42,12 +42,12 @@
             <div>
                 <div class="dropdown dropdown-end hidden lg:inline-block">
                     <label tabindex="0" class="btn m-1 grey-color rounded-3xl px-5" @click="toggleDropdown" @blur="">
-                        {{sortOptions[selectedSortFilter]}}
+                        {{ sortOptions[selectedSortFilter] }}
                     </label>
                     <ul tabindex="0" class="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52">
                         <li v-for="key of Object.keys(sortOptions)">
                             <label class="label cursor-pointer">
-                                <span class="label-text">{{sortOptions[key]}}</span>
+                                <span class="label-text">{{ sortOptions[key] }}</span>
                                 <input type="radio" name="radioSortFilter" class="radio" :value="key"
                                     v-model="selectedSortFilter" />
                             </label>
@@ -56,12 +56,12 @@
                 </div>
                 <div class="dropdown dropdown-end hidden lg:inline-block">
                     <label tabindex="0" class="btn m-1 grey-color rounded-3xl px-5" @click="toggleDropdown" @blur="">
-                        {{sortDirection[selectedSortDirection]}}
+                        {{ sortDirection[selectedSortDirection] }}
                     </label>
                     <ul tabindex="0" class="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52">
                         <li v-for="key in Object.keys(sortDirection)">
                             <label class="label cursor-pointer">
-                                <span class="label-text">{{sortDirection[key]}}</span>
+                                <span class="label-text">{{ sortDirection[key] }}</span>
                                 <input type="radio" name="radioSortDirection" class="radio" :value="key"
                                     v-model="selectedSortDirection" />
                             </label>
@@ -74,12 +74,11 @@
         </div>
         <div class="grid gap-6 mt-3 lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 xs:grid-cols-1">
             <template v-if="recipesToDisplay !== null">
-                <div v-for="recipe in recipesToDisplay" class="w-full">
-                    <RecipeCard :recipe="recipe" @click="navigateTo(`/recipes/${recipe.id}`)" />
-                </div>
+                <RecipeCard v-for="recipe in recipesToDisplay" :recipe="recipe"
+                    @click="navigateTo(`/recipes/${recipe.id}`)" />
             </template>
         </div>
-        <button class="btn w-full grey-color rounded-3xl mt-5" @click="offset+=4">Show More</button>
+        <button class="btn w-full grey-color rounded-3xl mt-5" @click="offset += 4">Show More</button>
     </div>
 </template>
 
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="section p-5 max-w-6xl m-auto">
-    <div class="flex flex-col md:flex-row gap-[100px] min-h-[400px] p-10 justify-center">
+    <div class="flex flex-col md:flex-row md:flex-row gap-[100px] min-h-[400px] sm:p-10 justify-center">
       <div class="self-center">
         <h1 class="text-5xl font-black">NOM.NOM</h1>
         <div class="py-6 text-slate-600">
@@ -14,29 +14,34 @@
           <nuxt-link to="/fridge"><button class="btn btn-ghost">My Fridge ›</button></nuxt-link>
         </div>
       </div>
-      <div class="w-full lg:w-[40%] rounded-3xl relative">
+      <div class="w-full md:w-[40%] rounded-3xl relative">
         <template v-if="recipesOfTheDay !== null">
-          <div v-for="(recipe, index) in recipesOfTheDay.recipes" class="w-full sm:w-[400px] mx-auto" :key="index">
-            <div v-show="currentCard === index"
-              class="card bg-base-100 p-2 border rounded-2xl hover:cursor-pointer hover:shadow-md ease-in duration-150 mt-2">
-              <img class="h-full sm:h-[300px] object-cover rounded-lg" :src="recipe.image" alt="" />
-              <div class="card-body pt-4 pb-2 px-2 gap-0">
-                <p class="text-[#d54215] font-bold text-xs uppercase">Today's special</p>
-                <h2 class="card-title truncate block text-md">{{recipe.title}}</h2>
-                <p class="text-gray-500 text-sm">
-                  {{recipe.extendedIngredients.length}} Ingredients
-                </p>
-                <p class="text-gray-500 text-sm">
-                  {{recipe.readyInMinutes}} Minutes
-                </p>
+          <Carousel>
+            <Slide v-for="(recipe, index) in recipesOfTheDay.recipes" :key="index">
+              <div class="w-full mx-auto">
+                <nuxt-link :to="`/recipes/${recipe.id}`">
+                  <div
+                    class="card bg-base-100 p-2 border rounded-2xl hover:cursor-pointer hover:shadow-md ease-in duration-150 mt-2">
+                    <img class="h-[200px] object-cover rounded-lg" :src="recipe.image" alt="" />
+                    <div class="card-body pt-4 pb-2 px-2 gap-0">
+                      <p class="text-[#d54215] font-bold text-xs uppercase">Today's special</p>
+                      <h2 class="card-title truncate block text-md w-[95%] mx-auto">{{ recipe.title }}</h2>
+                      <p class="text-gray-500 text-sm">
+                        {{ recipe.extendedIngredients.length }} Ingredients
+                      </p>
+                      <p class="text-gray-500 text-sm">
+                        {{ recipe.readyInMinutes }} Minutes
+                      </p>
+                    </div>
+                  </div>
+                </nuxt-link>
               </div>
-            </div>
-          </div>
-          <div class="flex justify-center items-center space-x-5 mt-5">
-            <div v-for="(_, index) in recipesOfTheDay.recipes" class="dot w-2 h-2 rounded-full cursor-pointer"
-              :class="[(currentCard === index) ? 'bg-black': 'bg-[#cbcbcb]']" @click="updateCurrentCard(index)">
-            </div>
-          </div>
+            </Slide>
+            <template #addons>
+              <navigation />
+              <pagination />
+            </template>
+          </Carousel>
         </template>
       </div>
     </div>
@@ -44,10 +49,8 @@
       <h1 class="text-5xl font-bold">Popular recipes</h1>
       <div class="grid gap-6 mt-2 lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 xs:grid-cols-1">
         <template v-if="popularRecipesInfo !== null">
-          <div v-for="recipe in Object.values(popularRecipesInfo)" class="w-full">
-            <RecipeCard :showIngredients="true" :showPreparationTime="true" :recipe="recipe"
-              @click="navigateTo(`/recipes/${recipe.id}`)" />
-          </div>
+          <RecipeCard v-for="recipe in Object.values(popularRecipesInfo)" :showIngredients="true"
+            :showPreparationTime="true" :recipe="recipe" @click="navigateTo(`/recipes/${recipe.id}`)" />
         </template>
       </div>
       <nuxt-link to="/explore">
@@ -58,9 +61,9 @@
 </template>
 
 <script setup>
+import 'vue3-carousel/dist/carousel.css'
 import { useApiStore } from "~/store/apiStore";
-
-
+import { Carousel, Slide, Pagination, Navigation } from 'vue3-carousel'
 
 const apiStore = useApiStore();
 let recipesOfTheDay = ref(null);
@@ -70,12 +73,6 @@ let popularRecipesID = computed(() => {
   return popularRecipes.value.results.map((recipe) => recipe.id);
 })
 
-const currentCard = ref(0);
-
-const updateCurrentCard = (index) => {
-  currentCard.value = index;
-}
-
 onBeforeMount(async () => {
   popularRecipes.value = await getPopularRecipes(apiStore.apiIndex, 4, 0);
   recipesOfTheDay.value = await getRandomRecipe(apiStore.apiIndex, 4);
@@ -83,3 +80,24 @@ onBeforeMount(async () => {
 });
 
 </script>
+
+<style >
+.carousel__pagination-button {
+  background-color: #090f207f;
+  margin-right: 10px;
+  margin-left: 10px;
+  border-radius: 50%;
+}
+
+.carousel__pagination-button--active {
+  background-color: #000;
+}
+
+.carousel__pagination-button::after {
+  content: none;
+}
+
+.carousel__pagination {
+  margin-top: 20px;
+}
+</style>
