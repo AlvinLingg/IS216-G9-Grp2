612a57cc46944da4733956b4f921dac03b3c0db8
its-seb <51190277+its-seb@users.noreply.github.com>
2022-11-10 19:36:26 +0800

    Liked recipes breakpoint change

--- pages/profile/[uid].vue
+++ pages/profile/[uid].vue
@@ -5,10 +5,12 @@
     </div>
     <div v-else class="section p-10 max-w-6xl m-auto">
       <div class="flex gap-5 justify-center">
-        <div v-if="userProfile.profilePicture === undefined"
-          class="inline-flex overflow-hidden relative justify-center items-center w-[100px] h-[100px] bg-gray-100 rounded-full dark:bg-gray-600">
+        <div
+          v-if="userProfile.profilePicture === undefined"
+          class="inline-flex overflow-hidden relative justify-center items-center w-[100px] h-[100px] bg-gray-100 rounded-full dark:bg-gray-600"
+        >
           <span class="font-medium text-gray-600 dark:text-gray-300">{{
-              userProfile.profileHandle.slice(0, 5)
+            userProfile.profileHandle.slice(0, 5)
           }}</span>
         </div>
         <div v-else>
@@ -16,7 +18,10 @@
         </div>
 
         <div class="self-center">
-          <h1 v-if="userProfile.displayName === undefined" class="text-3xl font-bold">
+          <h1
+            v-if="userProfile.displayName === undefined"
+            class="text-3xl font-bold"
+          >
             {{ userProfile.profileHandle }}
           </h1>
           <h1 v-else class="text-3xl font-bold">
@@ -24,10 +29,13 @@
           </h1>
           <p class="text-sm text-slate-600">@{{ userProfile.profileHandle }}</p>
 
-          <div v-if="
-            userStore.user !== null &&
-            userStore.user.profileHandle === userProfile.profileHandle
-          " class="flex gap-3 mt-3">
+          <div
+            v-if="
+              userStore.user !== null &&
+              userStore.user.profileHandle === userProfile.profileHandle
+            "
+            class="flex gap-3 mt-3"
+          >
             <button class="btn" @click="navigateTo('/profile/edit')">
               Edit Profile
             </button>
@@ -36,25 +44,41 @@
       </div>
 
       <div class="tabs font-semibold mt-10">
-        <a @click="toggleTab(0)" :class="{
-          'tab text-3xl tab-active': openTab === 0,
-          'tab text-3xl': openTab !== 0,
-        }">My recipes</a>
-        <a @click="toggleTab(1)" :class="{
-          'tab text-3xl tab-active': openTab === 1,
-          'tab text-3xl': openTab !== 1,
-        }">Liked recipes</a>
+        <a
+          @click="toggleTab(0)"
+          :class="{
+            'tab text-3xl tab-active': openTab === 0,
+            'tab text-3xl': openTab !== 0,
+          }"
+          >My recipes</a
+        >
+        <a
+          @click="toggleTab(1)"
+          :class="{
+            'tab text-3xl tab-active': openTab === 1,
+            'tab text-3xl': openTab !== 1,
+          }"
+          >Liked recipes</a
+        >
       </div>
 
-      <div :class="{
-        'h-[100px] mt-10 px-[1rem]': openTab === 0,
-        'h-[100px] mt-10 px-[1rem] hidden': openTab !== 0,
-      }">
-        <div v-if="userStore.user" class="text-center p-16 bg-[#f3f4f6] rounded-3xl">
+      <div
+        :class="{
+          'h-[100px] mt-10 px-[1rem]': openTab === 0,
+          'h-[100px] mt-10 px-[1rem] hidden': openTab !== 0,
+        }"
+      >
+        <div
+          v-if="userStore.user"
+          class="text-center p-16 bg-[#f3f4f6] rounded-3xl"
+        >
           <h1 class="text-3xl font-bold">Oops!</h1>
           <p class="mt-3">Looks like you have not created any recipes.</p>
           <p>Have a recipe you wanna share?</p>
-          <button class="btn rounded-3xl primary-color mt-3" @click="navigateTo('/recipes/create')">
+          <button
+            class="btn rounded-3xl primary-color mt-3"
+            @click="navigateTo('/recipes/create')"
+          >
             Upload Recipe
           </button>
         </div>
@@ -69,22 +93,36 @@
           </p>
         </div>
       </div>
-      <div :class="{
-        'h-[100px] bg-blue mt-10 px-[1rem]': openTab === 1,
-        'h-[100px] bg-blue mt-10 px-[1rem] hidden': openTab !== 1,
-      }">
-        <div v-if="userStore.user" class="text-center p-10 bg-[#f3f4f6] rounded-3xl">
+      <div
+        :class="{
+          'h-[100px] bg-blue mt-10 px-[1rem]': openTab === 1,
+          'h-[100px] bg-blue mt-10 px-[1rem] hidden': openTab !== 1,
+        }"
+      >
+        <div
+          v-if="userStore.user"
+          class="text-center p-10 bg-[#f3f4f6] rounded-3xl"
+        >
           <div v-if="favoritedRecipes.length == 0">
             <h1 class="text-3xl font-bold">Oops!</h1>
             <p class="mt-3">Looks like you have not liked any recipes.</p>
             <p>Let&#39;s go explore some recipes!</p>
-            <button class="btn rounded-3xl primary-color mt-3" @click="navigateTo('/')">
+            <button
+              class="btn rounded-3xl primary-color mt-3"
+              @click="navigateTo('/')"
+            >
               Discover Recipes
             </button>
           </div>
-          <div class="grid gap-4 mt-3 lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 xs:grid-cols-1" v-else>
-            <RecipeCard v-for="recipe in Object.values(recipesToDisplay)" :recipe="recipe"
-              @click="navigateTo(`/recipes/${recipe.id}`)" />
+          <div
+            class="grid gap-4 mt-3 lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1"
+            v-else
+          >
+            <RecipeCard
+              v-for="recipe in Object.values(recipesToDisplay)"
+              :recipe="recipe"
+              @click="navigateTo(`/recipes/${recipe.id}`)"
+            />
           </div>
         </div>
 
@@ -144,7 +182,10 @@ if (userProfile.value != undefined) {
   };
 
   favoritedRecipes.value = await getUserFavorites();
-  recipesToDisplay.value = await getRecipeInformationBulk(apiStore.apiIndex, favoritedRecipes.value.map(x => x.rid).join(','));
+  recipesToDisplay.value = await getRecipeInformationBulk(
+    apiStore.apiIndex,
+    favoritedRecipes.value.map((x) => x.rid).join(",")
+  );
   console.log(recipesToDisplay.value);
 }
 </script>
