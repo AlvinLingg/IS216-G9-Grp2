de5fb24a9233cd58b703dfb7c0d4002f64b2d01d
its-seb <51190277+its-seb@users.noreply.github.com>
2022-11-10 18:40:56 +0800

    Change favorite button CSS

--- components/FoodCarousel.vue
+++ components/FoodCarousel.vue
@@ -78,53 +78,74 @@ const handleClick = async () => {
 <template>
   <div>
     <div class="card bg-base-100 shadow-xl w-full sm:w-4/5 md:w-3/5 lg:w-full">
-      <figure>
+      <figure class="relative">
         <img :src="props.imageURL" />
-      </figure>
-      <div class="card-body">
-        <h2 class="card-title">
-          {{ props.recipes.title }}
-          <div class="badge cursor-pointer bg-transparent" :class="{
+        <div
+          class="rounded-full p-4 cursor-pointer bg-white absolute right-6 top-6"
+          :class="{
             'text-red-600': liked,
             'hover:text-gray-600': liked,
             'text-gray-600': !liked,
             'hover:text-red-600': !liked,
-          }" @click="handleClick">
-            <heart-solid class="text-xs" />
-            <span class="pl-1" v-if="!liked">Favorite</span>
-            <span class="pl-1" v-else>Favorited</span>
-          </div>
+          }"
+          @click="handleClick"
+          style="box-shadow: -2px 8px 15px -7px rgba(0, 0, 0, 0.46)"
+        >
+          <heart-solid class="text-xl" />
+        </div>
+      </figure>
+      <div class="card-body">
+        <h2 class="card-title">
+          {{ props.recipes.title }}
         </h2>
         <p class="text-gray-500">By: {{ props.recipes.sourceName }}</p>
         <div class="card-actions mt-5">
-          <div class="badge badge-outline" v-if="
-            props.recipes.vegetarian && props.recipes.vegetarian.length != 0
-          ">
+          <div
+            class="badge badge-outline"
+            v-if="
+              props.recipes.vegetarian && props.recipes.vegetarian.length != 0
+            "
+          >
             Vegetarian
           </div>
-          <div class="badge badge-outline" v-if="props.recipes.vegan && props.recipes.vegan.length != 0">
+          <div
+            class="badge badge-outline"
+            v-if="props.recipes.vegan && props.recipes.vegan.length != 0"
+          >
             Vegan
           </div>
-          <div class="badge badge-outline" v-if="
-            props.recipes.glutenFree && props.recipes.glutenFree.length != 0
-          ">
+          <div
+            class="badge badge-outline"
+            v-if="
+              props.recipes.glutenFree && props.recipes.glutenFree.length != 0
+            "
+          >
             Gluten-Free
           </div>
-          <div class="badge badge-outline" v-if="
-            props.recipes.dairyFree && props.recipes.dairyFree.length != 0
-          ">
+          <div
+            class="badge badge-outline"
+            v-if="
+              props.recipes.dairyFree && props.recipes.dairyFree.length != 0
+            "
+          >
             Dairy-Free
           </div>
-          <div class="badge badge-outline" v-if="
-            props.recipes.sustainable && props.recipes.sustainable.length != 0
-          ">
+          <div
+            class="badge badge-outline"
+            v-if="
+              props.recipes.sustainable && props.recipes.sustainable.length != 0
+            "
+          >
             Sustainable
           </div>
         </div>
       </div>
     </div>
-    <div class="toast toast-end" :class="{ hidden: hideToast }">
-      <div class="alert alert-error">
+    <div
+      class="toast toast-center justify-center"
+      :class="{ hidden: hideToast }"
+    >
+      <div class="alert alert-error w-[325px] justify-center">
         <div>
           <span>Please log in to favorite recipes!</span>
         </div>
@@ -135,6 +156,4 @@ const handleClick = async () => {
 
 <script></script>
 
-<style>
-
-</style>
+<style></style>
